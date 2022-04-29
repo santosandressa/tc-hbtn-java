e45c588d0a5bcde6125812395a39dcbf03d3aa4d
santosandressa <andressasantosp0@gmail.com>
2022-04-29 16:53:38 -0300

    task 1 - Print Integer Variable

---
+++ 0x00-java-hello_world/PrintIntegerVariable.java
@@ -0,0 +1,10 @@
+public class PrintIntegerVariable {
+
+    public static void main(String[] args) {
+        String rua = "Avenida Andaló, ";
+
+        int numero = 311;
+
+        System.out.println(rua + numero);
+    }
+}
