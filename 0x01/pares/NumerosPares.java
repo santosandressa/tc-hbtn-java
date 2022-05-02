b284cfbd1d4cee86315e4111f85ea37a2def3383
santosandressa <andressasantosp0@gmail.com>
2022-05-02 11:19:10 -0300

    task 1 - números pares

---
+++ 0x01/pares/NumerosPares.java
@@ -0,0 +1,13 @@
+public class NumerosPares {
+    public static void main(String[] args) {
+        for (int i = 0; i < 98; i++) {
+            System.out.printf(i + ", ");
+
+            if (i == 96){
+                System.out.println("98");
+            }
+
+            i++;
+        }
+    }
+}
