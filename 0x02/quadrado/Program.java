74e9482e8d05f60137f2fb780441bb07eec77bde
santosandressa <andressasantosp0@gmail.com>
2022-05-03 10:08:36 -0300

    task 1 - Área Quadrado

---
+++ 0x02/quadrado/Program.java
@@ -0,0 +1,7 @@
+package testes;
+public class Program {
+    public static void main(String[] args) {
+       double lado = -3;
+       System.out.println(Quadrado.area(lado));
+    }
+}
---
+++ 0x02/quadrado/Quadrado.java
@@ -0,0 +1,18 @@
+package testes;
+
+public class Quadrado {
+    public static double area(double lado) {
+        double resultado = 0;
+        try {
+            resultado = lado * lado;
+
+            if (lado < 0) {
+                throw new IllegalArgumentException();
+            }
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("Lado deve possuir valor positivo");
+        }
+        return resultado;
+    }
+}
