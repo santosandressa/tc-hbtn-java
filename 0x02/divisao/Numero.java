47bd2f6eda3824d127e17dee14260c2c5a46a28e
santosandressa <andressasantosp0@gmail.com>
2022-05-03 09:31:16 -0300

    task 0 - Divisão inteira

---
+++ 0x02/divisao/Numero.java
@@ -0,0 +1,12 @@
+class Numero {
+    public static void dividir(int a, int b) {
+        int resultado = 0;
+        try {
+            resultado = a / b;
+        } catch (ArithmeticException e) {
+            System.out.println("Nao eh possivel dividir por zero");
+        } finally {
+            System.out.println(a + " / " + b + " = " + resultado);
+        }
+    }
+}
