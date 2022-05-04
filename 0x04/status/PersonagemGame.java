a5e422ade8a959fbae4b9d50fc952c8b8875fbd8
santosandressa <andressasantosp0@gmail.com>
2022-05-04 16:35:16 -0300

    task 3 - status do personagemn

--- 0x04/status/PersonagemGame.java
+++ 0x04/status/PersonagemGame.java
@@ -1,16 +1,21 @@
-package metodos;
 
 public class PersonagemGame {
 
     private int saudeAtual;
     private String nome;
 
+    private String status;
+
     public int getSaudeAtual() {
         return saudeAtual;
     }
 
     public void setSaudeAtual(int saudeAtual) {
-        this.saudeAtual = saudeAtual;
+        if (this.saudeAtual > 0) {
+            this.status = "vivo";
+        } else {
+            this.status = "morto";
+        }
     }
 
     public String getNome() {
@@ -21,12 +26,17 @@ public class PersonagemGame {
         this.nome = nome;
     }
 
+    public String getStatus() {
+        return status;
+    }
+
     public int tomarDano(int quantidadeDeDano){
         this.saudeAtual -= quantidadeDeDano;
 
         if (this.saudeAtual <= 0){
             this.saudeAtual = 0;
         }
+        setSaudeAtual(this.saudeAtual);
 
         return this.saudeAtual;
     }
@@ -38,7 +48,8 @@ public class PersonagemGame {
             this.saudeAtual = 100;
         }
 
+        setSaudeAtual(this.saudeAtual);
+
         return  this.saudeAtual;
     }
-
 }
