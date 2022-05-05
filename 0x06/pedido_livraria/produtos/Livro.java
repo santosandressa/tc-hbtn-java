67ce78e68ebff0e66c8de2fa31ef57277e45f89a
santosandressa <andressasantosp0@gmail.com>
2022-05-05 17:04:33 -0300

    task 0. Pedido Livraria

--- 0x06/pedido_livraria/produtos/Livro.java
+++ 0x06/pedido_livraria/produtos/Livro.java
@@ -6,8 +6,6 @@ public class Livro extends Produto{
     private String autor;
     private int edicao;
 
-
-
     public Livro(String titulo, int ano, String pais, double precoBruto, int paginas, String autor, int edicao) {
         super(titulo, ano, pais, precoBruto);
         this.paginas = paginas;
@@ -15,6 +13,29 @@ public class Livro extends Produto{
         this.edicao = edicao;
     }
 
+    public int getPaginas() {
+        return paginas;
+    }
+
+    public void setPaginas(int paginas) {
+        this.paginas = paginas;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getEdicao() {
+        return edicao;
+    }
+
+    public void setEdicao(int edicao) {
+        this.edicao = edicao;
+    }
 
     @Override
     public double obterPrecoLiquido() {
