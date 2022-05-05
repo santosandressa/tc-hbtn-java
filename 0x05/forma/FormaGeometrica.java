b1c0232520af1d9213a124e6cd6cdc51d6b30265
santosandressa <andressasantosp0@gmail.com>
2022-05-05 11:35:32 -0300

    task 1 - forma geometrica

--- 0x05/forma/FormaGeometrica.java
+++ 0x05/forma/FormaGeometrica.java
@@ -1,6 +1,6 @@
 public class FormaGeometrica {
     public double area(){
-        throw new UnsupportedOperationException("Funcao area nao implemetada.");
+        throw new UnsupportedOperationException("Funcao area nao implemetada");
 
 
     }
