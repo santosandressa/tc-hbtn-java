6e3357de1f4a0d687380de6e4fbcd4956ce2761e
santosandressa <andressasantosp0@gmail.com>
2022-05-05 17:19:46 -0300

    task 0. Pedido Livraria

--- 0x06/pedido_livraria/Program.java
+++ 0x06/pedido_livraria/Program.java
@@ -4,7 +4,7 @@ import java.util.Locale;
 public class Program {
     public static void main(String[] args) {
         Locale.setDefault(new Locale("pt", "BR"));
-        Lprodutos.Livro livro1 = new produtos.Livro("Duna", 2017, "Brasil", 56.30, 680, "Frank Herbert", 1);
+        produtos.Livro livro1 = new produtos.Livro("Duna", 2017, "Brasil", 56.30, 680, "Frank Herbert", 1);
         produtos.Livro livro2 = new produtos.Livro("Um de nós está mentindo", 2018, "Brasil", 34.93, 384,
                 "Karen Mcmanus", 1);
         produtos.Livro livro3 = new produtos.Livro("Mindset Milionário", 2021, "Brasil", 31.70, 272,
