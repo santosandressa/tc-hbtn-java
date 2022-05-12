67f4bfa816e38aff94d1ceec029f1d4187c6d081
santosandressa <andressasantosp0@gmail.com>
2022-05-12 11:15:49 -0300

    task 1 cookies

---
+++ List/cookies/Pedido.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class Pedido {
+
+    private ArrayList<PedidoCookie> cookies;
+
+    public Pedido(){
+        this.cookies = new ArrayList<>();
+    }
+
+    public void adicionarPedidoCookie(PedidoCookie cookie){
+        this.cookies.add(cookie);
+    }
+
+
+    public int obterTotalCaixas() {
+        int total = 0;
+
+        for (PedidoCookie cookie : this.cookies) {
+            total += cookie.getQuantidadeCaixas();
+        }
+
+        return total;
+
+    }
+
+    public int removerSabor(String sabor) {
+        int total = 0;
+
+        for (int i = 0; i < this.cookies.size(); i++) {
+            if (this.cookies.get(i).getSabor().equals(sabor)) {
+                total += this.cookies.get(i).getQuantidadeCaixas();
+                this.cookies.remove(i);
+            }
+        }
+
+        return total;
+    }
+
+}
--- List/numeros/ManipularArrayNumeros.java
+++ List/numeros/ManipularArrayNumeros.java
@@ -1,3 +1,5 @@
+package numeros;
+
 import java.util.List;
 
 public class ManipularArrayNumeros {
