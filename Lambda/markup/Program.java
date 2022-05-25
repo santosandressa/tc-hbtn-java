f39e8863b4b6d76198e02282c492f772c935dfac
santosandressa <andressasantosp0@gmail.com>
2022-05-25 13:55:26 -0300

    0. Produto MarkUp

--- Lambda/markup/Program.java
+++ Lambda/markup/Program.java
@@ -1,5 +1,5 @@
 public class Program {
-    public static void main(String[] args)
+    public static void main(String[] args){
         Produto produto1 = new Produto(129.99, "Mouse Gamer");
 
         System.out.printf("%s - %.2f - %.2f\n", produto1.getNome(),
