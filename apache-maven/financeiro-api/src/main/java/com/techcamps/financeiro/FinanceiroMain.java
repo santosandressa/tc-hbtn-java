392c84576178ebc8130c634a4e320cd78a354f68
santosandressa <andressasantosp0@gmail.com>
2022-06-01 14:00:44 -0300

    Task 0 - apache maven

---
+++ apache-maven/financeiro-api/src/main/java/com/techcamps/financeiro/FinanceiroMain.java
@@ -0,0 +1,7 @@
+package com.techcamps.financeiro;
+
+public class FinanceiroMain {
+    public static void main(String[] args) {
+        System.out.println("Criando um projeto Java Maven.");
+    }
+}
